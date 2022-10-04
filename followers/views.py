f10dd7c1c2acf812959cba3b74d10d90c3fa4828
Stian Lystad <Stianlystad_1@hotmail.com>
2022-10-04 11:11:55 +0000

    changes to posts and profiles

--- followers/views.py
+++ followers/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
 from followers.models import Follower
-from followers.serializers import FollowerSerializer
+from followers.serializer import FollowerSerializer
 
 
 class FollowerList(generics.ListCreateAPIView):
